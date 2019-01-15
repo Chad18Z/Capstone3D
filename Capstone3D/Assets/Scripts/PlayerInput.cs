96a893749af59394098f83c346f2f2c8b7a99ec9
Chad18Z <credeurchad@gmail.com>
2019-01-14 22:39:53 -0700

    Player ship 3D model axes aligned with main camera.

--- Capstone3D/Assets/Scripts/PlayerInput.cs
+++ Capstone3D/Assets/Scripts/PlayerInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// Receive input from the player
@@ -13,7 +14,8 @@ public class PlayerInput : MonoBehaviour
     GameObject gunTorus;
 
     // Raycasting for mouse position. We need a plane for the ray to intersect with
-    public Plane groundPlane;
+    [SerializeField]
+    GameObject groundPlane;
 
     // How fast the player's ship rotates when the mouse is moved
     float rotationSpeed = 10f;
@@ -22,17 +24,14 @@ public class PlayerInput : MonoBehaviour
     Rigidbody rb;
 
     // How fast the ship accelerates
-    float speed = 5f;
+    float speed = 3f;
 
     // The ship's velocity, initialize to zero
     Vector3 moveVelocity = Vector3.zero;
 
-    // Rotation of the ship
-    Quaternion rot;
-
-    // Hold input
-    float hInput; // horizontal axis
-    float vInput; // vertical axis
+    // Reference to camera needed for raycasting into scene
+    [SerializeField]
+    Camera mainCamera;
 
     #endregion
 
@@ -42,33 +41,34 @@ public class PlayerInput : MonoBehaviour
     {
         gunTorus = GameObject.FindGameObjectWithTag("gunTorus");
         rb = gameObject.GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
     private void Update()
     {
         // Get input values from each axis
-        hInput = Input.GetAxisRaw("Horizontal");
-        vInput = Input.GetAxisRaw("Vertical");
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
 
         // Velocity vector from player input
         moveVelocity = new Vector3(vInput * speed, 0, hInput * speed);
 
         // The cannons should always face the mouse cursor
         // Shoot a ray from the camera to the mouse cursor
-        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        float rayLength;
-
-       // if (groundPlane.Raycast
-
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 lookDirection = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            gunTorus.transform.LookAt(lookDirection);
+        }
     }
 
     // Apply physics to game objects
     void FixedUpdate()
     {
-        rb.velocity = moveVelocity;
+        rb.velocity = moveVelocity;         
     }
 
     #endregion
