ef1c5c8679b3d0e3aaeb27447c9893495161bebb
Chad18Z <credeurchad@gmail.com>
2019-01-16 23:17:58 -0700

    Added code for rotation easing. Not yet functional.

--- Capstone3D/Assets/Scripts/Anvil.cs
+++ Capstone3D/Assets/Scripts/Anvil.cs
@@ -23,6 +23,9 @@ public class Anvil : MonoBehaviour
     // Distance from the player character in which the mouse cursor is detectable
     float mouseRadius = 2.05f;
 
+    // State machine support
+    AnvilStates state = AnvilStates.IDLE;
+
     #endregion
 
     #region Methods
@@ -45,10 +48,56 @@ public class Anvil : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, hit.point) >= mouseRadius)
             {
-                Vector3 pointToLookAt = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-                gunTorus.transform.LookAt(pointToLookAt);
+                if (state == AnvilStates.INVISIBLE)
+                {
+                    // The mouse cursor is outside of the player character's radius, let's change the state to "easing"
+                    state = AnvilStates.EASING;
+
+                    // Store the ship's current rotation. We'll send this to the Coroutine
+                    float currentAngle = gunTorus.transform.eulerAngles.y;
+
+                    // Calculate the direction from the ship to the mouse cursor. We'll need this to calculate the angle to rotate towards
+                    Vector3 targetDirection = hit.point - transform.position;
+
+                    // Calculate the target angle IN DEGREES 
+                    float angle = Mathf.Atan2(targetDirection.z, targetDirection.x) * Mathf.Deg2Rad;
+
+                    float targetAngle = angle;
+
+                    StartCoroutine(RotateShip(currentAngle, targetAngle));
+                }
+                else
+                {
+                    Vector3 pointToLookAt = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                    gunTorus.transform.LookAt(pointToLookAt);
+                }
+            }
+            else if (state == AnvilStates.IDLE)
+            {
+                state = AnvilStates.INVISIBLE;
+                // Here I will add the code to make the anvil "disappear" in front of the player character
             }
         }
+    }
+
+    /// <summary>
+    /// Coroutine to smoothly rotate the ship
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator RotateShip(float currentAngle, float targetAngle)
+    {
+        Vector3 temp = transform.eulerAngles;
+        for (float f = 0; f <= 1; f += .1f)
+        {
+            //gunTorus.transform.rotation = Quaternion.Slerp(currentAngle, targetAngle, f);
+            //float ease = EaseOutBounceD(currentAngle, targetAngle, f);
+            //temp.y = ease;
+            //gunTorus.transform.rotation = Quaternion.Euler(temp);
+            yield return null;
+        }
+
+        // Easing is complete. Change state back to IDLE
+        state = AnvilStates.IDLE;
     }
 
     /// <summary>
---
+++ Capstone3D/Assets/Scripts/AnvilStates.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// States of the anvil
+/// </summary>
+public enum AnvilStates 
+{
+    IDLE,
+    INVISIBLE,
+    EASING
+}
